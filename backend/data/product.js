865c5c2c00b65a95d2fb4b0f9357ecc54c227966
soma33333 <soma3333aa@gmail.com>
2025-10-28 09:59:51 +0530

    added images url to products

--- backend/data/product.js
+++ backend/data/product.js
@@ -2,31 +2,31 @@ module.exports = [
   {
     name: 'Classic Cotton T-Shirt',
     price: 399.0,
-    image: "",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzQRmuhVVQ5H7VQVIhvACXITzmsa0jNF_kJQ&s",
     description: 'Comfortable cotton t-shirt available in multiple sizes.'
   },
   {
     name: 'Wireless Earbuds',
     price: 1299.0,
-    image: '',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhSxxf2HMnYkf3up490eA8EJsyudL4GPLqVA&s',
     description: 'Bluetooth 5.0 with charging case.'
   },
   {
     name: 'Stainless Steel Water Bottle',
     price: 799.0,
-    image: '',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvdJPwCNORA6caIhnF6DcoGUex1L2D1hN5cw&s',
     description: 'Keeps drinks cold for 24 hours.'
   },
   {
     name: 'Slim Laptop Sleeve 13 inch',
     price: 599.0,
-    image: '',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0r8Zs8IpzIotTyCXu9zSN3BS91bP7h7EQJA&s',
     description: 'Protective sleeve for laptops and tablets.'
   },
   {
     name: 'Desk Lamp - LED',
     price: 899.0,
-    image: '',
+    image: 'https://m.media-amazon.com/images/I/51DcV4YJw2L._AC_UF1000,1000_QL80_.jpg',
     description: 'Energy-efficient LED desk lamp with adjustable head.'
   }
 ]
